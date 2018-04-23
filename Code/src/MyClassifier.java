0ce2ca6c7ab252271e7fa0b7d57c2f7f5689e62a
nzho8446 <nzho8446@uni.sydney.edu.au>
2018-04-24 03:25:42 +1000

    added some documentation

--- Code/src/MyClassifier.java
+++ Code/src/MyClassifier.java
@@ -3,6 +3,19 @@ import java.io.File;
 
 public class MyClassifier {
 
+
+  /**
+   * Constructor marked private to prevent instantiation outside of this class.
+   */
+  private MyClassifier() {
+
+  }
+
+  /**
+   * The main function reads in the arguments of from the command line.
+   * @param args There are 3 arguments expected. args[0] is the training file, args[1] is the test file args[2] is the algorithm to be run.
+   *
+   */
   public static void main(String[] args) {
     if(args.length != 3)
       return;
