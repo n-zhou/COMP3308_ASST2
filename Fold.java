4a9b1ed831612db8cc719c37dd00e42c4fa5a2a2
nzho8446 <nzho8446@uni.sydney.edu.au>
2018-04-29 04:13:46 +1000

    added results

--- Fold.java
+++ Fold.java
@@ -3,6 +3,7 @@ import java.io.PrintWriter;
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.LinkedList;
+import java.util.Comparator;
 
 /**
  * XXX
@@ -18,6 +19,7 @@ public class Fold {
      */
     @SuppressWarnings("unchecked")
     public static LinkedList[] read() {
+        LinkedList<String> input = new LinkedList<>();
         LinkedList<String>[] folds = new LinkedList[10];
         for(int i = 0; i < folds.length; ++i) {
             folds[i] = new LinkedList<>();
@@ -27,7 +29,17 @@ public class Fold {
             Scanner sc = new Scanner(file);
             int k = 0;
             while (sc.hasNextLine()) {
-                folds[(k++) % 10].add(sc.nextLine());
+                input.add(sc.nextLine());
+            }
+            input.sort(new Comparator<String>() {
+
+                public int compare(String s1, String s2) {
+                    return s1.split(",")[8].compareTo(s2.split(",")[8]);
+                }
+
+            });
+            while (!input.isEmpty()) {
+                folds[(k++) % 10].add(input.poll());
             }
 
         } catch (IOException e) {
